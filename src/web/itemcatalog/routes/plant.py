d535046a2ff015027489ed9c16245f87f1fa4860
eric-yates <eric@medleyagency.com>
2022-01-17 23:40:26 -0700

    Return status code

--- src/web/itemcatalog/routes/plant.py
+++ src/web/itemcatalog/routes/plant.py
@@ -65,7 +65,7 @@ def create_plant():
 
     print(f'''Added plant with name "{r['name']}"!''')
 
-    return 'Success!'
+    return {"status_code": 200}
 
 
 @plant.route("/plant/water", methods=['POST'])
@@ -86,3 +86,5 @@ def water_plant():
 
     db.session.add(new_water_entry)
     db.session.commit()
+
+    return {"status_code": 200}
