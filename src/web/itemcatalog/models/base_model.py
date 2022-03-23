1dfbb7f163f0dc59b285bfde033a2eb1d7d87d91
eric-yates <eric@medleyagency.com>
2022-03-22 23:32:02 -0600

    Rename dir

--- src/web/itemcatalog/models/base_model.py
+++
@@ -1,9 +0,0 @@
-from . import db
-
-
-class BaseModel(db.Model):
-    __abstract__ = True
-
-    def to_dict(self):
-        """Return a dictionary representation of this model."""
-        return {c.name: getattr(self, c.name) for c in self.__table__.columns}
