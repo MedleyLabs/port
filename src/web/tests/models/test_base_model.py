714f109579fb3badec14a78e2b012f6a5b4df65c
eric-yates <eric@medleyagency.com>
2022-05-25 00:03:49 -0600

    Create carbon offset plugin

--- src/web/tests/models/test_base_model.py
+++ src/web/tests/models/test_base_model.py
@@ -1,11 +1,10 @@
 import unittest
 
-from port.models.base_model import BaseModel
+from server.models.base_model import BaseModel
 
 
 class DummyModel(BaseModel):
     pass
-
 
 
 class TestBaseModel(unittest.TestCase):
@@ -13,7 +12,8 @@ class TestBaseModel(unittest.TestCase):
     dummy_model = DummyModel()
 
     def test_base_model_plugin_name(self):
-        self.assertTrue(self.dummy_model.plugin_name == 'tests.models')
+        print(self.dummy_model.plugin_name)
+        self.assertTrue(self.dummy_model.plugin_name == 'tests')
 
     def test_base_model_plugin_id(self):
         self.assertTrue(self.dummy_model.plugin_id == 1)
