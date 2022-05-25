ead37969d6188f030339fce63d62bf594adfc935
eric-yates <eric@medleyagency.com>
2022-05-25 00:41:57 -0600

    Move to port

---
+++ src/web/port/__init__.py
@@ -0,0 +1,52 @@
+import os
+import sys
+
+from flask import Flask
+from flask_login import LoginManager
+from flask_marshmallow import Marshmallow
+from flask_sqlalchemy import SQLAlchemy
+
+login_manager = LoginManager()
+db = SQLAlchemy()
+ma = Marshmallow()
+
+
+def initialize_app():
+    """ Initializes the Flask app """
+
+    app = Flask(__name__)
+
+    app.config.from_object('config.settings.Development')  # TODO
+
+    login_manager.init_app(app)
+    db.init_app(app)
+    ma.init_app(app)
+
+    with app.app_context():
+
+        # Built-in routes
+        from port.routes.category import category
+        from port.routes.errorhandlers import errorhandlers
+        from port.routes.main import main
+        from port.routes.plugin import plugin
+        from port.routes.userauth import userauth
+
+        # Import routes here
+
+        # Built-in blueprints
+        app.register_blueprint(category)
+        app.register_blueprint(errorhandlers)
+        app.register_blueprint(main)
+        app.register_blueprint(plugin)
+        app.register_blueprint(userauth)
+
+        # Register blueprints here
+
+    return app
+
+
+app = initialize_app()
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=8000)
