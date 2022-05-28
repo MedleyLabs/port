bc82cc26810b6185ec0560b35314b77d3ee7c099
eric-yates <eric@medleyagency.com>
2022-05-28 04:09:08 -0600

    Account for the well-to-tank emissions factor

--- src/web/port/carbon_offset/models.py
+++ src/web/port/carbon_offset/models.py
@@ -28,11 +28,29 @@ class GasolinePurchase(BaseModel):
 
 
 class CarbonEmission(BaseModel):
-    """ An amount of carbon emitted from burning gasoline """
+    """
+    An amount of carbon emitted from burning gasoline
+
+    Source below for the well-to-tank emissions factor, which is how much CO2 is produced from the following process:
+
+        1. Extracting crude oil from a well
+        2. Shipping the crude oil to a refinery
+        3. Refining the crude oil into gasoline
+        4. Shipping the gasoline to a gas station
+
+    https://innovationorigins.com/en/producing-gasoline-and-diesel-emits-more-co2-than-we-thought/
+    """
 
     __tablename__ = 'carbon_emission'
 
+    liters_per_gallon = 3.7854
+    grams_per_pound = 453.5924
+    well_to_tank_grams_co2_per_liter = 720
+
+    well_to_tank_pounds_co2_per_gallon = well_to_tank_grams_co2_per_liter * liters_per_gallon / grams_per_pound
+
     pounds_gasoline_per_gallon = 6.3
+
     carbon_atomic_weight = 12
     oxygen_atomic_weight = 16
     co2_atomic_weight = carbon_atomic_weight + 2 * oxygen_atomic_weight
@@ -43,11 +61,11 @@ class CarbonEmission(BaseModel):
     gasoline_purchase_id = db.Column(db.Integer, db.ForeignKey('gasoline_purchase.id'))
 
     @classmethod
-    def create(cls, number_of_gallons, octane=87, gasoline_purchase_id=None):
+    def create(cls, gallons, octane=87, gasoline_purchase_id=None):
 
         pounds_carbon_per_gallon = cls.pounds_gasoline_per_gallon * octane/100
         pounds_co2_per_gallon = pounds_carbon_per_gallon * cls.co2_to_carbon_weight_ratio
-        pounds_co2 = pounds_co2_per_gallon * number_of_gallons
+        pounds_co2 = gallons * (pounds_co2_per_gallon + cls.well_to_tank_pounds_co2_per_gallon)
 
         print(f'Carbon emission: {pounds_co2} pounds CO2')
 
